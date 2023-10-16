f16150370e42116f76034857f6b5e75f96245cbd
Rafa <rafa.bediaz@gmail.com>
2023-10-15 21:00:26 -0600

    Add: logo (ico, png) and new products. change cards products style, change home page style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+// const navigation = document.querySelector("nav");
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > 20) {
+//         navigation.classList.add('cambiar-color');
+//   } else {
+//         navigation.classList.remove('cambiar-color')
+//   }
+// });
+
 AOS.init({
     easing: 'ease-out-back',
     duration: 1000
 });
+
