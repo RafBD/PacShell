8509883c593d141ce0b691e3b2b21bc4fe7da2fe
Rafa <rafa.bediaz@gmail.com>
2023-11-28 08:46:30 -0600

    General changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// const navigation = document.querySelector("nav");
-// window.addEventListener("scroll", function () {
-//   if (window.scrollY > 20) {
-//         navigation.classList.add('cambiar-color');
-//   } else {
-//         navigation.classList.remove('cambiar-color')
-//   }
-// });
-
 AOS.init({
     easing: 'ease-out-back',
     duration: 1000
